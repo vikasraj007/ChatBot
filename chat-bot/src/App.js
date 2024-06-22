438184e2ad884ab3fb8886e759caf5c742951540
vikasraj007 <vikasvikky738@gmail.com>
2024-06-22 23:44:47 +0530

    initial commit for chatbot

---
+++ chat-bot/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './ChatbotMain'
+import ChatbotMain from './ChatbotMain';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      <ChatbotMain name="vikas"></ChatbotMain>
+    </div>
+  );
+}
+
+export default App;
